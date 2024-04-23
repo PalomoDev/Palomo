6fa1a74e754e73454c988ebc80459ba6afed19ae
serge-palomo <sergey.palomo@gmail.com>
2024-04-23 17:50:52 +0200

    second mobile commit++

--- src/app/ui/mobile/home-page.tsx
+++ src/app/ui/mobile/home-page.tsx
@@ -15,18 +15,21 @@ export const HomePageMobile = () => {
    const windowSize = useClientMediaQuery()
 
     console.log(windowSize.width, windowSize.height)
+    const windowHeight = windowSize.height
 
 
     return (
         <>
-            <div><p>{windowSize.width}</p><p>{windowSize.height}</p></div>
-            <section className={`w-full  min-h-[100vh] max-h-[100vh] bg-[#0C0601] grid grid-rows-[auto,1fr]`}>
+
+            <section className={`w-full  min-h-screen bg-[#0C0601] grid grid-rows-[auto,1fr]`}>
                 <div className={'h-24 border-b'}>
                     <HeaderMobile/>
                 </div>
 
 
-                <div className={'bg-[url(/cover-mobile/image_58.png)] pb-6 bg-cover flex items-end justify-center'}>
+                <div className={`bg-[url(/cover-mobile/image_58.png)]  pb-6 bg-cover flex items-end justify-center`} style={{
+                    height: windowHeight-90
+                }}>
                     <FooterMobile/>
 
                 </div>
